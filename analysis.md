df448c35cb5455a0bffca31337235f2f18d482f4
bilal <bilalqwider@yahoo.com>
2022-12-30 04:20:32 +0200

    blog

---
+++ analysis.md
@@ -0,0 +1,53 @@
+property:
+  -name 
+  -main img
+  -images /slider
+  -price
+  -decription
+  -place
+  -#features:
+  -rating:
+    room rating 
+    add rate [book room]
+    room -->avg rating 
+  -book:
+   book
+   check avaiblity
+  -related 
+
+users
+
+
+blog:
+ title 
+ tag 
+ img
+ created at
+ author
+ decs
+ category
+
+ comments:
+
+
+about:
+img
+what we do
+our mission
+our goals
+
+
+faq:
+title
+decsription
+
+
+
+websaite:
+ adress
+ phone
+ email
+ myname 
+ my logo
+ desc about me
+ social link
--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'settings',
+    'about',
+    'property',
+    'blog',
+    'taggit',
+    'django_summernote',
+    'bootstrap4',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +64,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL ='/static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+
+X_FRAME_OPTIONS='SAMEORIGIN'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
